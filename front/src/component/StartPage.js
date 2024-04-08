655801c227473d857f8ada75c29342136013cb64
Dmytro Kiperman <8boyss@ukr.net>
2024-04-08 19:37:18 +0200

    upd

--- front/src/component/StartPage.js
+++ front/src/component/StartPage.js
@@ -7,14 +7,16 @@ import welcome from "./bank-image.svg"
 const StartPage = () => {
   return (
     <div className="start-page">
-      <div>
-        <h1>Hello!</h1>
-        <p>Welcome to the Bank App</p>
+      <div className='start-page__background-image'></div>
+      <div className='start-page__text'>
+        <h1 className='start-page__text-title'>Hello!</h1>
+        <p className='start-page__text-description'>Welcome to Bank App</p>
       </div>
-      <img src={welcome} alt="Bank Image" />
-      <div className="buttons">
-        <button>Sign Up</button>
-        <button>Sign In</button>
+      <img src={welcome} alt="Bank Image" className='start-page__image'/>
+        
+      <div className="start-page__buttons">
+        <button className='start-page__button-purple'>Sign Up</button>
+        <button className='start-page__button-white'>Sign In</button>
       </div>
     </div>
   );
