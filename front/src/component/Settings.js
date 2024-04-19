ccff04173e0a7e0b60fa490e71f69d14c9491763
Dmytro Kiperman <8boyss@ukr.net>
2024-04-19 18:50:48 +0200

    upd

--- front/src/component/Settings.js
+++ front/src/component/Settings.js
@@ -33,24 +33,43 @@ const Settings = () => {
 
   return (
     <div className='page'>
-      <header>
+      <div className='header'>
         <BackButton onClick={handleBackButtonClick}/>
-      </header>
+        <h1 className='header__title'>Settings</h1>
+      </div>
 
       <form className='form' onSubmit={handleSubmit}>
-        <h1 className='form__title'>Settings</h1>
+        {/* <h1 className='form__title'>Settings</h1> */}
         
         <div className='field'>
           <h6 className='field__title'>Change email</h6>  
-          <label className='field__label' type="email" name="email">Email:</label>
+          <label className='field__label' type="email" name="email">Email</label>
           <input className='field__input' type="email" value={email} placeholder='example@gmail.com' onChange={handleEmailChange} />
         </div>
         <div className='field'>
-          <label className='field__label'>Old Password:</label>
+          <label className='field__label'>Old Password</label>
           <input className='field__input' type="password" value={password} placeholder='********' onChange={handlePasswordChange} />
         </div>
 
         <button className='form__button' type="submit">Save Email</button>
+
+        <hr className='divider'/>
+
+        <div className='field'>
+          <h6 className='field__title'>Change password</h6>  
+          <label className='field__label' type="password" name="password">Old Password</label>
+          <input className='field__input' type="email" value={email} placeholder='example@gmail.com' onChange={handleEmailChange} />
+        </div>
+        <div className='field'>
+          <label className='field__label'>New Password</label>
+          <input className='field__input' type="password" value={password} placeholder='********' onChange={handlePasswordChange} />
+        </div>
+
+        <button className='form__button' type="submit">Save Password</button>
+
+        <hr className='divider'/>
+
+        <button className='form__button' type="submit">Log out</button>
         
       </form>
     </div>
