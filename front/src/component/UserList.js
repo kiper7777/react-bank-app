7e4da414bc4d0484ac5ead27f5da8c48b8162650
Dmytro Kiperman <8boyss@ukr.net>
2024-08-10 16:08:14 +0200

    upd

--- front/src/component/UserList.js
+++ front/src/component/UserList.js
@@ -20,7 +20,7 @@ const UsersList = () => {
 
   return (
     <div className='user-list-container'>
-      <h1>Registered Users</h1>
+      <h1 className='user-list-title'>Registered Users:</h1>
       <table>
         <thead>
           <tr>
@@ -44,53 +44,3 @@ const UsersList = () => {
 export default UsersList;
 
 
-// import React, { useState, useEffect } from 'react';
-// import './UserList.css';
-
-// const UserList = () => {
-//   const [users, setUsers] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     // Fetch data from backend when component mounts
-//     const fetchUsers = async () => {
-//       try {
-//         const response = await fetch('http://localhost:4000/users');
-//         if (!response.ok) {
-//           throw new Error('Failed to fetch users');
-//         }
-//         const data = await response.json();
-//         setUsers(data);
-//       } catch (error) {
-//         setError(error.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchUsers();
-//   }, []);
-
-//   return (
-//     <div className="user-list-container">
-//       <h2>Registered users</h2>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : error ? (
-//         <p className="error">{error}</p>
-//       ) : (
-//         <ul className="user-list">
-//           {users.map((user) => (
-//             <li key={user.id} className="user-item">
-//               <span className="user-name">{user.name}</span>
-//               <span className="user-email">{user.email}</span>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default UserList;
